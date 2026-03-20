be651cc73337ec0e4b677eafcd81acfdf523792e
“luiswave-es” <luiswaveflow@gmail.com>
2026-03-20 15:47:32 +0100

    Añado cambio Back pone estado servicio cuando estado de todos sus platos servidos

--- backend/src/main/java/com/gestionqr/backend/service/PedidoService.java
+++ backend/src/main/java/com/gestionqr/backend/service/PedidoService.java
@@ -13,67 +13,70 @@ import com.gestionqr.backend.model.Pedido.EstadoPedido;
 import com.gestionqr.backend.model.Servicio;
 import com.gestionqr.backend.model.Servicio.EstadoServicio;
 import com.gestionqr.backend.repository.PedidoRepository;
+import com.gestionqr.backend.repository.ServicioRepository;
 
 @Service
 public class PedidoService {
-	
+
 	@Autowired
 	PedidoRepository pedidoRepository;
 	
-	public List<Pedido> obtenerPedidosPorEstado(EstadoPedido estado){
+	@Autowired
+	ServicioRepository servicioRepository;
+
+	public List<Pedido> obtenerPedidosPorEstado(EstadoPedido estado) {
 		return pedidoRepository.findByEstado(estado);
 	}
-	
-    public Pedido crearPedido(@RequestBody Pedido pedido) {    	
-    	return pedidoRepository.save(pedido);
-    }    
-    
-    public Pedido cambiarEstado(Long id) {
 
-        Pedido pedido = pedidoRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Pedido no encontrado"));
-        
-        if (pedido.getServicio().getEstado() == EstadoServicio.Cerrado) {
-            throw new RuntimeException("El servicio ya está cerrado");
-        }
+	public Pedido crearPedido(@RequestBody Pedido pedido) {
+		return pedidoRepository.save(pedido);
+	}
 
-        EstadoPedido actual = pedido.getEstado();
-        EstadoPedido nuevoEstado;
-        
-        switch (actual) {
-        case Pendiente -> nuevoEstado = EstadoPedido.EnProceso;
-        case EnProceso -> nuevoEstado= EstadoPedido.Listo;
-        case Listo -> nuevoEstado = EstadoPedido.Servido;
-		default -> throw new IllegalArgumentException("Unexpected value: " + actual);
-        }
+	public Pedido cambiarEstado(Long id) {
 
-        pedido.setEstado(nuevoEstado);
+		Pedido pedido = pedidoRepository.findById(id).orElseThrow(() -> new RuntimeException("Pedido no encontrado"));
 
-        Pedido actualizado = pedidoRepository.save(pedido);
-        
-        Servicio servicio = pedido.getServicio();
-        if (servicio != null) {
-            comprobarYCerrarServicio(servicio);
-        }
+		if (pedido.getServicio().getEstado() == EstadoServicio.Cerrado) {
+			throw new RuntimeException("El servicio ya está cerrado");
+		}
 
-        return actualizado;
-    }
-    
-    private void comprobarYCerrarServicio(Servicio servicio) {
+		EstadoPedido actual = pedido.getEstado();
+		EstadoPedido nuevoEstado;
 
-        List<Pedido> pedidos = pedidoRepository.findByServicioId(servicio.getId());
+		switch (actual) {
+			case Pendiente -> nuevoEstado = EstadoPedido.EnProceso;
+			case EnProceso -> nuevoEstado = EstadoPedido.Listo;
+			case Listo -> nuevoEstado = EstadoPedido.Servido;
+			default -> throw new IllegalArgumentException("Unexpected value: " + actual);
+		}
 
-        boolean todosServidos = pedidos.stream()
-                .allMatch(p -> p.getEstado() == EstadoPedido.Servido);
+		pedido.setEstado(nuevoEstado);
 
-        if (todosServidos) {
-            servicio.setEstado(EstadoServicio.Cerrado);
-        }
-    }
-    
-    public List<Pedido> obtenerActivos() {
-        return pedidoRepository.findByEstadoIn(
-            List.of(EstadoPedido.Pendiente, EstadoPedido.EnProceso, EstadoPedido.Listo)
-        );
-    }
+		Pedido actualizado = pedidoRepository.save(pedido);
+
+		Servicio servicio = pedido.getServicio();
+		if (servicio != null) {
+			comprobarYCerrarServicio(servicio);
+		}
+
+		return actualizado;
+	}
+
+	private void comprobarYCerrarServicio(Servicio servicio) {
+
+		List<Pedido> pedidos = pedidoRepository.findByServicioId(servicio.getId());
+
+		boolean todosServidos = pedidos.stream().allMatch(p -> p.getEstado() == EstadoPedido.Servido);
+
+		if (todosServidos) {
+			servicio.setEstado(EstadoServicio.Cerrado);
+			servicioRepository.save(servicio);
+		}
+		
+	}
+
+	public List<Pedido> obtenerActivos() {
+		return pedidoRepository
+				.findByEstadoIn(List.of(EstadoPedido.Pendiente, EstadoPedido.EnProceso, EstadoPedido.Listo));
+	}
 }
