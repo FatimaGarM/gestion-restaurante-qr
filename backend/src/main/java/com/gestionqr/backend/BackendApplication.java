caa9d202973747f6bd26f06ad58e56a0430c9d7f
“luiswave-es” <luiswaveflow@gmail.com>
2026-04-27 05:11:15 +0200

    Modifico Docker-compose y añado init.sql

--- backend/src/main/java/com/gestionqr/backend/BackendApplication.java
+++ backend/src/main/java/com/gestionqr/backend/BackendApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("com.gestionqr.backend.model")
 public class BackendApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder  builder) {
