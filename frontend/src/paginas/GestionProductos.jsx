b40ccd5342ebf1b2a344a0278c144662a3639478
“luiswave-es” <luiswaveflow@gmail.com>
2026-04-29 19:48:58 +0200

    Añado cambio

--- frontend/src/paginas/GestionProductos.jsx
+++ frontend/src/paginas/GestionProductos.jsx
@@ -297,7 +297,7 @@ function GestionProductos() {
                     >
                         {t("proveedores.añadirProveedor")}
                     </button>
-                    <div className="flex items-center align-middle gap-2">
+                    <div className="flex items-center align-middle al gap-2">
                         <div className="flex-col items-center gap-2">
                             <span className="block text-sm font-medium text-gray-700 mb-2">{t("proveedores.editar")}</span>
                             <select
@@ -313,7 +313,7 @@ function GestionProductos() {
                                 ))}
                             </select>
                         </div>
-                        <div className="mt-2 flex items-center gap-2">
+                        <div className="mt-2 flex items-center align-middle gap-2">
                             <div className="flex-col items-center align-middle gap-2">
                                 <span className="block text-sm font-medium text-gray-700 mb-2">{t("proveedores.eliminar")}</span>
                                 <select
