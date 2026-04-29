006f54cbc6159f8e09e3a31c007cd78abd5edbc9
“luiswave-es” <luiswaveflow@gmail.com>
2026-04-29 20:02:15 +0200

    Añado cambio

--- frontend/src/paginas/GestionProductos.jsx
+++ frontend/src/paginas/GestionProductos.jsx
@@ -314,7 +314,7 @@ function GestionProductos() {
                             </select>
                         </div>
                         <div className="flex items-center align-middle gap-2">
-                            <div className="flex-col items-center align-middle gap-2">
+                            <div className="flex items-center gap-2">
                                 <span className="block text-sm font-medium text-gray-700 mb-2">{t("proveedores.eliminar")}</span>
                                 <select
                                     value={proveedorBorrar}
